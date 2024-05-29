165aac321abaca109c6b4de83134a572cce7791e
icarodredd <icarog854@gmail.com>
2024-05-29 14:32:01 -0300

    feat: add footer

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,13 +4,13 @@ import { Button } from "./ui/button";
 
 export default function Header() {
   return (
-    <div className="flex justify-between p-4">
+    <section className="flex justify-between p-8">
       <Image
         className="rounded-full size-14 shadow-xl hover:shadow-none"
         src={k}
         alt="Kodis Logo"
       />
       <Button className="rounded-full font-bold">Join Waitlist</Button>
-    </div>
+    </section>
   );
 }
