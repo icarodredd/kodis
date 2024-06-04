d9a95cebc8222ff19cecfe8a9fc04b1839879448
icarodredd <icarog854@gmail.com>
2024-06-04 09:02:20 -0300

    feat: modal done

--- components/WaitlistModal.tsx
+++ components/WaitlistModal.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "./ui/button";
 import {
   Card,
@@ -7,15 +8,45 @@ import {
   CardHeader,
   CardTitle,
 } from "./ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "./ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  email: z.string().email(),
+});
 
 export default function WaitlistModal({ setClick }: any) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <div className="fixed h-screen inset-0 flex justify-center items-center backdrop-blur-md">
-      <Card className="w-1/3">
+      <Card className="w-1/3 max-lg:w-5/6">
         <div className="flex justify-between">
           <CardHeader>
             <CardTitle>Join Waitlist</CardTitle>
-            <CardDescription>Enter your best email</CardDescription>
+            <CardDescription>Enter your best email.</CardDescription>
           </CardHeader>
           <Button onClick={() => setClick(false)} variant="ghost">
             X
@@ -23,11 +54,28 @@ export default function WaitlistModal({ setClick }: any) {
         </div>
 
         <CardContent>
-          <p>Card Content</p>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Type here..." {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      You will receive a email after join waitlist.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
         </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
       </Card>
     </div>
   );
